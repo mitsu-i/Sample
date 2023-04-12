2978454c75eb26f944d679b6644d4515fbe7f2a7
mitsu-i <mitsui116.a@gmail.com>
2023-04-13 00:40:03 +0900

    テストで追加

--- Sample.java
+++ Sample.java
@@ -39,7 +39,7 @@ public final class Sample extends JavaPlugin implements Listener {
     // Plugin startup logic
     Bukkit.getPluginManager().registerEvents(this, this);
 
-    List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15);
+    List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15123);
 
     numberList.stream()
         .mapToInt(number -> number)
